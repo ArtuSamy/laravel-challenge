5bbeb0bd27afacb7c24975b2d891042a5e9f101d
ArtuSamy <hamg_18@hotmail.com>
2020-06-26 00:33:52 -0500

    EventoApp

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+        });
+
+            Schema::create('eventos', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('title');
+                $table->foreign('title')->references('id')->on('users');
+                $table->text('description');
+                $table->datetime('dataI');
+                $table->datetime('dataF');
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('eventos');
+    }
+}
